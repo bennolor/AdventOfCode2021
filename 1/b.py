9611f8fc2cf3cdb95645e6a7cbbe353403e282b1
benno <bennolor@gmail.com>
2021-12-01 22:33:24 +0100

    renamed var

--- 1/b.py
+++ 1/b.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
-    x = 0
+    result = 0
     with open("a_in.txt", "r") as my_in:
         depth_list = list(map(lambda x: int(x), my_in.readlines()))
 
     for i in range(len(depth_list)):
         window_one_start = i
         if (sum(depth_list[i:i + 3])) < (sum(depth_list[i + 1:i + 4])):
-            x += 1
+            result += 1
 
-    print(f"found {x} instances where the following window was bigger")
+    print(f"found {result} instances where the following window was bigger")
