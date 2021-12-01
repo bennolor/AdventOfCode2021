1ad63aa3384e0ff03b5dce0b6b5132199f16196a
benno <bennolor@gmail.com>
2021-12-01 23:14:43 +0100

    LUL

--- 1/b.py
+++ 1/b.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
-    result = 0
     with open("a_in.txt", "r") as my_in:
         depth_list = list(map(lambda x: int(x), my_in.readlines()))
 
-    for i in range(len(depth_list)):
-        if (sum(depth_list[i:i + 3])) < (sum(depth_list[i + 1:i + 4])):
-            result += 1
-
+    result = [sum(depth_list[i:i+3]) < sum(depth_list[i+1:i+4]) for i in range(len(depth_list))].count(True)
     print(f"found {result} instances where the following window was bigger")
