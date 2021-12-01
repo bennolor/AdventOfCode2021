f54e6c55b808ad0cc5cbcb38d3a66816e1166e98
benno <bennolor@gmail.com>
2021-12-01 22:47:58 +0100

    formatting

--- 1/a.py
+++ 1/a.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     with open("a_in.txt", "r") as my_in:
         depth_list = list(map(lambda x: int(x), my_in.readlines()))
 
-    result = list(map(lambda a: a[0]<a[1], list(itertools.pairwise(depth_list)))).count(True)
+    result = list(map(lambda a: a[0] < a[1], list(itertools.pairwise(depth_list)))).count(True)
     print(f"There are {result} measurements that are larger than the previous one")
