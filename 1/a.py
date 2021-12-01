1ce941366eb723e60123abe61d2a8eeaa0da519b
benno <bennolor@gmail.com>
2021-12-01 22:47:37 +0100

    optimized answer a

--- 1/a.py
+++ 1/a.py
@@ -1,11 +1,8 @@
+import itertools
+
 if __name__ == '__main__':
-    a = float("inf")
-    b = 0
-    x = 0
-    with open("a_in.txt","r") as my_in:
-        for line in my_in:
-            b = int(line)
-            if a < b:
-                x += 1
-            a = b
-    print(f"There are {x} measurements that are larger than the previous one")
+    with open("a_in.txt", "r") as my_in:
+        depth_list = list(map(lambda x: int(x), my_in.readlines()))
+
+    result = list(map(lambda a: a[0]<a[1], list(itertools.pairwise(depth_list)))).count(True)
+    print(f"There are {result} measurements that are larger than the previous one")
