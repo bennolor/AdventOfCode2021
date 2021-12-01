ebd58139c7860de4c5a9ef55ec56106974a3cbe5
benno <bennolor@gmail.com>
2021-12-01 21:50:40 +0100

    Initial commit

---
+++ 1/a.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    a = float("inf")
+    b = 0
+    x = 0
+    with open("a_in.txt","r") as my_in:
+        for line in my_in:
+            b = int(line)
+            if a < b:
+                x += 1
+            a = b
+    print(f"There are {x} measurements that are larger than the previous one")
