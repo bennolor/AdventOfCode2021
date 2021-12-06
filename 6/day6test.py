adec4411c5bdbc6504fb546c42c767448513e806
benno <bennolor@gmail.com>
2021-12-06 12:21:37 +0100

    day 6 part 2 Test

--- 6/day6test.py
+++ 6/day6test.py
@@ -1,11 +1,21 @@
 import unittest
 
-from day6 import solution1
+from day6 import solution1, solution2
+
 
 class TestAoC(unittest.TestCase):
+    test_input = '3,4,3,1,2'
+
     def test_part_1(self):
         """
-        Testing Part 1 of AoC
+        Testing Part 1 of AoC Day 5 Challenge by using the given example
         :return
         """
-        self.assertEqual(5934, solution1('3,4,3,1,2'))
+        self.assertEqual(5934, solution1(self.test_input))
+
+    def test_part_2(self):
+        """
+        Testing Part 1 of AoC Day 5 Challenge by using the given example
+        :return:
+        """
+        self.assertEqual(26984457539, solution2(self.test_input))
