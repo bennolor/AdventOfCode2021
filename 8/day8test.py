5acd0f230e9988362ffcaa84b26f9179f4f0c0e0
benno <bennolor@gmail.com>
2021-12-08 15:28:43 +0100

    fixed wrong input in test

--- 8/day8test.py
+++ 8/day8test.py
@@ -1,12 +1,14 @@
 import unittest
 from day8 import solution1, solution2
 
+
 class TestAoC(unittest.TestCase):
-    test_input = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
+    with open('testin.txt', 'r') as test_input_file:
+        test_input = test_input_file.readlines()
 
     def test_part_1(self):
         """
         Testing Day 8 Part one
         :return:
         """
-        self.assertEqual(26,solution1(self.test_input))
+        self.assertEqual(26, solution1(self.test_input))
