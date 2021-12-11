5fcffa23c0392ac5d571b41f699358d01b581479
benno <bennolor@gmail.com>
2021-12-11 03:39:32 +0100

    wrote day 1 part 1 test

---
+++ 10/day10test.py
@@ -0,0 +1,13 @@
+import unittest
+try:
+    from day10 import solution1, solution2
+except ImportError:
+    pass
+
+
+class TestAoC(unittest.TestCase):
+    with open("testin.txt", "r") as input_file:
+        test_input = input_file.readlines()
+
+    def test_solution1(self):
+        self.assertEqual(26397, solution1(self.test_input))
